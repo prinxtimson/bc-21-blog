5457419c1526a9579b5b373ca1b89169dcde0c51
Timson Babajide <timson.babajide@gmail.com>
2017-04-20 03:11:28 +0100

    javascript form validation

--- bootstrap/js/main.js
+++ bootstrap/js/main.js
@@ -1,18 +1,36 @@
 function validateInput(){
+    let item = document.publishPost;
 
-	if(document.publishPost.name.value == ''){
+	if(item.name.value == ''){
 		alert('Please enter your name');
-		document.publichPost.name.focus();
+		item.name.focus();
 		return false;
-	}else if(document.publishPost.title.value == ''){
+	}else if(item.title.value == ''){
 		alert('Enter your Title');
-		document.publichPost.title.focus();
+		item.title.focus();
 		return false;
-	}else if(document.publishPost.post.value == ''){
+	}else if(item.post.value == ''){
 		alert('Please tell us your story.');
-		document.publichPost.post.focus();
+		item.post.focus();
 		return false;
 	}else{
-
+        if(localStorage.getItem("testJSON") == null){
+            var myItem = {"blogPost":[{"name":item.name.value, 
+                                    "title":item.title.value,
+                                    "post":item.post.value}]};
+            var myJSON = JSON.stringify(myItem);
+            localStorage.setItem("testJSON", myJSON);
+        }else{
+            var text =localStorage.getItem("testJSON");
+            var info = JSON.parse(text);
+            info.blogPost.push({"name":item.name.value, 
+                                "title":item.title.value,
+                                "post":item.post.value});
+            myJSON = JSON.stringify(info);
+            localStorage.setItem("testJSON", myJSON);
+        }
     }
 }
+
+
+
