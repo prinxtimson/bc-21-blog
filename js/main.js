c6fa8cb5014330c8f783964682e812c7be28d0a3
Timson Babajide <timson.babajide@gmail.com>
2017-04-21 01:28:16 +0100

    GUI remake

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 function redirect(){
     window.location="viewpost.html";
 }
+
+function timeDate(){
+    var currentTime = new Date();
+    return  (currentTime.getDate() + '-' + currentTime.getMonth()+1 + '-' +
+            currentTime.getFullYear() + ' @ ' + currentTime.getHours() + ':' +
+            currentTime.getMinutes() + ':' + currentTime.getSeconds());
+}
 function validateInput(){
     let item = document.publishPost;
 
@@ -17,9 +24,11 @@ function validateInput(){
 		item.post.focus();
 		return false;
 	}else{
+        var getTime = timeDate();
         if(localStorage.getItem("testJSON") == null){
             var myItem = {"blogPost":[{"name":item.name.value, 
                                     "title":item.title.value,
+                                    "time":getTime,
                                     "post":item.post.value}]};
             var myJSON = JSON.stringify(myItem);
             localStorage.setItem("testJSON", myJSON);
@@ -28,6 +37,7 @@ function validateInput(){
             var info = JSON.parse(text);
             info.blogPost.push({"name":item.name.value, 
                                 "title":item.title.value,
+                                "time":getTime,
                                 "post":item.post.value});
             myJSON = JSON.stringify(info);
             localStorage.setItem("testJSON", myJSON);
@@ -43,10 +53,32 @@ function listPost(){
         var info = JSON.parse(text);
         var output = '';
         for(var i = 0; i<info.blogPost.length; i++){
-            output += '<div class="well">' + '<p>' + info.blogPost[i]["name"] + '</p>'  + '<p>' + info.blogPost[i]["title"] + '</p>'
-                   + '<p>' + info.blogPost[i]["post"] + '</p>' + '</div>';
+            output += '<div id="articles">'+
+                        '<div>'+
+                            '<article>'+
+                                '<details>'+
+                                    '<summary><h3>' + info.blogPost[i]["title"] + '</h3></summary>'+
+                                    '<h>' + info.blogPost[i]["name"] + '</h><br>' + 
+                                    info.blogPost[i]["time"] + 
+                                    '<p>' + info.blogPost[i]["post"] + '</p>'+
+                                '</details>'+
+                            '</article>'+
+                        '</div>'+
+                        '<div id="icon">'+
+                            '<button type="button" onclick="deletePost('+ i + ')"><i class="fa fa-trash"></i></a>'+
+                        '</div>'+
+                    '</div>';
             }
         var update = document.getElementById('myPost');
         update.innerHTML = output
     } 
 }
+
+
+function deletePost(indexNum){
+    let info = JSON.parse(localStorage.getItem("testJSON"));
+    info.blogPost.splice(indexNum,indexNum);
+    let myJSON = JSON.stringify(info);
+    localStorage.setItem("testJSON", myJSON);
+}
+
